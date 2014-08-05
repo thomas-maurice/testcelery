04bebd5db15014b5783c52e050ab13e65ae87c8e
thomas <tmaurice59@gmail.com>
2014-08-05 14:20:18 +0200

    added a fail msg

--- mail/views.py
+++ mail/views.py
@@ -4,5 +4,8 @@ import utils.tasks
 
 # Create your views here.
 def home(request):
-    utils.tasks.send_email.delay("data :)")
-    return HttpResponse('woot !')
+    try:
+        utils.tasks.send_email.delay("data :)")
+        return HttpResponse('woot !')
+    except:
+        return HttpResponse('fail !')
